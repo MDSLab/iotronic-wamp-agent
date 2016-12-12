f6065cd7ec34f67ca4b8d7de1bd72fc7914a7953
Nicola Peditto <n.peditto@gmail.com>
2016-12-12 11:42:15 +0100

    autobahn.twisted sleep added

--- scenario/s4t-device/fake_device.py
+++ scenario/s4t-device/fake_device.py
@@ -1,39 +1,29 @@
 from autobahn.twisted.wamp import ApplicationSession, ApplicationRunner
 from twisted.internet.defer import inlineCallbacks, returnValue
-#from autobahn.twisted.util import sleep
+from autobahn.twisted.util import sleep
 
 class MyComponent(ApplicationSession):
-  
+
     @inlineCallbacks
     def onJoin(self, details):
         print("WAMP server session ready!")
 
-<<<<<<< HEAD
-	@inlineCallbacks
+        @inlineCallbacks
         def add(x, y):
-	    c = yield x+y
+            c = yield x+y
             returnValue(c)
-	
-	@inlineCallbacks
-        def hello(client_name, message):
-	    from random import randrange
-	    #s = randrange(1, 10)
-	    #sleep(s)
-	    result = yield "Hello by board to Conductor "+client_name+" that said me "+message
-=======
 
-        def add(x, y):
-            return x+y	
-	
+        @inlineCallbacks
         def hello(client_name, message):
-	    #print "DEVICE received from WAMP AGENT: "+str(args)
-	    result = "Hello by board to Conductor "+client_name+" that said me "+message
->>>>>>> 712f99b9d53304c891ad59b347912238469fe4c8
-	    print "DEVICE result: "+str(result)
-             
-            
+            from random import randrange
+            s = randrange(1, 10)
+            yield sleep(s)
+            result = "Hello by board to Conductor "+client_name+" that said me "+message+" - Time: "+str(s)
+            #result = yield "Hello by board to Conductor "+client_name+" that said me "+message
+            print "DEVICE result: "+str(result)
+		
             returnValue(result)
-	  
+
         try:
             yield self.register(add, u'com.myapp.add')
             yield self.register(hello, u'com.myapp.hello')
@@ -45,8 +35,3 @@ class MyComponent(ApplicationSession):
 if __name__ == '__main__':
     runner = ApplicationRunner(url=u"ws://192.168.17.1:8181/ws", realm=u"s4t")
     runner.run(MyComponent)
-    
-    
-    
-    
-    
